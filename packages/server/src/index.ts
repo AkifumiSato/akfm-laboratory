08b6a37ab8d0d42bdb7f3067c2e1f2328a834f07
akfm.sato <01047245@CF0286.local>
2024-02-05 10:56:34 +0900

    feature: validate env variables

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,35 +4,40 @@ import fastifyCookie from "@fastify/cookie";
 import fastifySession from "@fastify/session";
 import { githubAuthPlugin } from "./github-auth-plugin";
 import { healthCheckPlugin } from "./health-check-plugin";
+import * as v from "valibot";
 
 // prepare next app
 const nextApp = next({ dev: process.env.NODE_ENV !== "production" });
 const nextHandle = nextApp.getRequestHandler();
 await nextApp.prepare();
 
-// todo: validation
-const envSettings = {
-  sessionSecret: process.env.SESSION_SECRET as string,
-  oauth: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    },
-  },
-} as const;
+// validation of environment variables
+const envSchema = v.object({
+  SESSION_SECRET: v.string([v.minLength(32)]),
+  GITHUB_CLIENT_ID: v.string([v.minLength(1)]),
+  GITHUB_CLIENT_SECRET: v.string([v.minLength(1)]),
+});
+let envVariables: v.Input<typeof envSchema>;
+try {
+  envVariables = v.parse(envSchema, process.env);
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
 
+// prepare fastify
 const fastify = Fastify();
 
 fastify.register(healthCheckPlugin);
 fastify.register(fastifyCookie);
 fastify.register(fastifySession, {
   cookieName: "sessionId",
-  secret: envSettings.sessionSecret,
+  secret: envVariables.SESSION_SECRET,
 });
 fastify.register(githubAuthPlugin, {
   serveOrigin: "http://localhost:3000",
-  clientId: envSettings.oauth.github.clientId,
-  clientSecret: envSettings.oauth.github.clientSecret,
+  clientId: envVariables.GITHUB_CLIENT_ID,
+  clientSecret: envVariables.GITHUB_CLIENT_SECRET,
 });
 
 // todo: debug page remove
@@ -52,7 +57,7 @@ const PORT = 3000;
 try {
   await fastify.listen({ port: PORT });
 } catch (err) {
-  fastify.log.error(err);
+  console.error(err);
   process.exit(1);
 }
 
