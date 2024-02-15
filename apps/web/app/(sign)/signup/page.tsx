4dcb527080c37fba05d38862c06c5ca99e0b2776
佐藤昭文 <akfm.sato@gmail.com>
2024-02-15 22:37:30 +0900

    signup form validation

--- apps/web/app/(sign)/signup/page.tsx
+++ apps/web/app/(sign)/signup/page.tsx
@@ -2,24 +2,35 @@ import { redirect } from "next/navigation";
 import type { JSX } from "react";
 import { coreApiUrl } from "../../lib/api-url";
 import { SingUpPagePresentation } from "./presentation";
+import * as v from "valibot";
+
+const signupFormSchema = v.object({
+  name: v.string([v.minLength(1), v.maxLength(100)]),
+  email: v.string([v.email()]),
+  password: v.string([v.minLength(8), v.maxLength(100)]),
+});
 
 export default function SingUpPage(): JSX.Element {
   async function action(data: FormData) {
     "use server";
 
-    // todo: validation
     const user = {
       name: data.get("name"),
       email: data.get("email"),
       password: data.get("password"),
     };
+    const validatedUser = v.safeParse(signupFormSchema, user);
+    if (validatedUser.issues && validatedUser.issues.length > 0) {
+      console.error("issues", validatedUser.issues);
+      throw new Error("Invalid form data");
+    }
     // todo: fetcher
     const response = await fetch(`${coreApiUrl}/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify(validatedUser.output),
     });
 
     console.log("action", response.status, await response.json());
